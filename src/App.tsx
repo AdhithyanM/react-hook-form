d594cb4557bd4cfa624366ad924a965246f11b2d
AdhithyanM <adhithyanmanoharan26@gmail.com>
2023-10-27 07:06:34 +0530

    working with arrays and objects in react-hook-form inputs

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
 import "./App.css";
 
-import YouTubeFormV7 from "./components/YouTubeFormV7";
+import YouTubeFormV9 from "./components/YouTubeFormV9";
 
 function App() {
   return (
     <div>
-      <YouTubeFormV7 />
+      <YouTubeFormV9 />
     </div>
   );
 }
---
+++ src/components/YouTubeFormV9.tsx
@@ -0,0 +1,130 @@
+// having nested objects and arrays
+import { useForm } from "react-hook-form";
+import { DevTool } from "@hookform/devtools"; // to show the form state in devtools
+
+type FormValues = {
+  username: string;
+  email: string;
+  channel: string;
+  social: {
+    twitter: string;
+    facebook: string;
+  };
+  phoneNumbers: string[];
+};
+
+const YouTubeFormV9 = () => {
+  const form = useForm<FormValues>({
+    defaultValues: {
+      username: "Batman",
+      email: "",
+      channel: "",
+      social: {
+        twitter: "",
+        facebook: "",
+      },
+      phoneNumbers: ["", ""],
+    },
+  });
+
+  const { register, handleSubmit, formState, control } = form;
+  const { errors: formErrors } = formState;
+
+  const onSubmit = (formData: FormValues) => {
+    console.log("Form submitted", formData);
+  };
+
+  return (
+    <div>
+      <h1>YouTube Form</h1>
+
+      <div className="form-control">
+        <label className="username">Username</label>
+        <input
+          type="text"
+          id="username"
+          {...register("username", {
+            required: "Username is required",
+          })}
+        />
+        <p className="error">{formErrors.username?.message}</p>
+      </div>
+
+      <div className="form-control">
+        <label htmlFor="email">Email</label>
+        <input
+          type="email"
+          id="email"
+          {...register("email", {
+            required: "Email is required",
+            pattern: {
+              value:
+                /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/,
+              message: "Invalid email format",
+            },
+            validate: {
+              notAdmin: (fieldValue) => {
+                return (
+                  fieldValue !== "admin@example.com" ||
+                  "Enter a different email address"
+                );
+              },
+              notBlackListed: (fieldValue) => {
+                return (
+                  !fieldValue.endsWith("baddomain.com") ||
+                  "This domain is not supported"
+                );
+              },
+            },
+          })}
+        />
+        <p className="error">{formErrors.email?.message}</p>
+      </div>
+
+      <div className="form-control">
+        <label htmlFor="channel">Channel</label>
+        <input
+          type="text"
+          id="channel"
+          {...register("channel", {
+            required: {
+              value: true,
+              message: "Channel is required",
+            },
+          })}
+        />
+        <p className="error">{formErrors.channel?.message}</p>
+      </div>
+
+      <div className="form-control">
+        <label htmlFor="twitter">Twitter</label>
+        <input type="text" id="twitter" {...register("social.twitter")} />
+      </div>
+
+      <div className="form-control">
+        <label htmlFor="facebook">Facebook</label>
+        <input type="text" id="facebook" {...register("social.facebook")} />
+      </div>
+
+      <div className="form-control">
+        <label htmlFor="primary-phone">Primary phone number</label>
+        <input type="text" id="primary-phone" {...register("phoneNumbers.0")} />
+      </div>
+
+      <div className="form-control">
+        <label htmlFor="secondary-phone">Secondary phone number</label>
+        <input
+          type="text"
+          id="secondary-phone"
+          {...register("phoneNumbers.1")}
+        />
+      </div>
+
+      <button onClick={handleSubmit(onSubmit)}>Submit</button>
+
+      <DevTool control={control} />
+    </div>
+  );
+};
+
+export default YouTubeFormV9;
